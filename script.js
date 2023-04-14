b280697d337f3e60a2fe0a3ca7ace879af362bec
WhataD?0wD <ditakusuma66@gmail.com>
2023-04-14 22:28:32 +0700

    perbaikan

--- script.js
+++ script.js
@@ -1,56 +1,55 @@
 let alert = document.getElementById("warning");
 
-let formMessage = document.getElementById("formMesage");
-let submitForm = document.getElementById("submit-form");
+let formMessage = document.getElementById('formMesage')
+let submitForm = document.getElementById('submit-form')
 
-// const setNotifTimer = () => {
-//   alert.innerHTML = ``;
-// };
 
-const handleGetFormData = () => {
+const handleGetFormData = ()=>{
   var obj = {
-    name: document.getElementById("name").value,
-    email: document.getElementById("email").value,
-    city: document.getElementById("city").value,
-    zipCode: document.getElementById("zip-code").value,
-    message: document.getElementById("message").value,
-    status: document.getElementById("status").checked,
-  };
-  return obj;
-};
-
-const isNumber = (string) => {
-  if (isNaN(string) == false) {
-    return true;
-  } else {
-    return false;
+    name : document.getElementById("name").value,
+    email : document.getElementById("email").value,
+    city : document.getElementById("city").value,
+    zipCode : document.getElementById("zip-code").value,
+    status : document.getElementById("status").checked,
   }
-};
+  return obj
+}
 
-const checkboxIsChecked = () => {
-  if (document.getElementById("status").checked == true) {
-    return true;
-  } else {
-    return false;
+const isNumber = (string)=>{
+  if(isNaN(string)==false){
+    return true
   }
-};
-
-const validateFormData = (obj) => {
-  if (obj != null && isNumber(obj.zipCode) && checkboxIsChecked()) {
-    return true;
-  } else return false;
-};
-
-const submit = () => {
-  if (validateFormData(handleGetFormData()) == false) {
-    return (warning.innerHTML = `<div class = "notif notif-danger text-center">Periksa form anda sekali lagi</div>`);
-  } else {
-    let { city, email, message, name, status, zipCode } = handleGetFormData();
-    return (warning.innerHTML = `<div class = "notif notif-success text-center">Terimakasih <b>${name}</b>, kami akan menghubungi anda lebih lanjut</div>`);
+  else{
+    return false
   }
-};
+}
 
-submitForm.addEventListener("click", (e) => {
-  e.preventDefault();
-  submit();
-});
+const checkboxIsChecked = ()=>{
+  if(document.getElementById('status').checked == true){
+    return true
+  }
+  else{
+    return false
+  }
+}
+
+const validateFormData = (obj) =>{
+  if(obj != null && isNumber(obj.zipCode) && checkboxIsChecked()){
+    return true
+  }
+  else return false
+}
+
+const submit = ()=>{
+  if (validateFormData(handleGetFormData()) == false){
+    return warning.innerHTML = `<div class = "notif notif-danger text-center">Periksa form anda sekali lagi</div>`;
+  }
+  else{
+     return warning.innerHTML = ``;
+  }
+}
+
+submitForm.addEventListener('click',(e)=>{
+  e.preventDefault()
+  submit()
+})
